751da3aa8e94dc8bffbb2e554faeb772242ed227
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-04 19:43:10 -0300

    sidebar function

--- src/Hero.js
+++ src/Hero.js
@@ -9,6 +9,13 @@ const Hero = () => {
       <div className="hero-center">
         <article className="hero-info">
           <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ab,
+            possimus quaerat fuga exercitationem dolore nesciunt quasi illum
+            dolorum tempora vero reiciendis eius eaque rem sed magnam veritatis
+            tenetur eligendi!
+          </p>
+          <button className="btn">Start now</button>
         </article>
         <article className="hero-images">
           <img src={phoneImg} className="phone-img" alt="phone" />
