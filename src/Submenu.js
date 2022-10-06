dd4d7baa7d64545aa13d99c42a1cb4874fe11cbc
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-06 04:59:17 -0300

    map sublinks from data.js

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,7 +2,11 @@ import React, { useState, useRef, useEffect } from "react";
 import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  const { isSubmenuOpen, location } = useGlobalContext();
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
   const container = useRef(null);
 
   useEffect(() => {
@@ -17,7 +21,7 @@ const Submenu = () => {
       className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
       ref={container}
     >
-      submenu
+      <h4>{page}</h4>
     </aside>
   );
 };
